4ba0026431ce57869aa2bbc7152dc68a3f0f4d7f
ChrisTDB <christopher.orta101@gmail.com>
2025-11-24 13:02:18 -0500

    p3 - plugged in difficulty settings and initial dynamic settings for the alien invasion game

--- Lab14_corta1.py
+++ Lab14_corta1.py
@@ -67,6 +67,8 @@ class AlienInvasion:
         
         if self.alien_fleet.check_destroyed_status():
             self._reset_level()
+            self.settings.increase_difficulty()
+            #update game stats
         
     def _check_game_status(self):
         if self.game_stats.ships_left > 0:
@@ -82,6 +84,7 @@ class AlienInvasion:
         self.alien_fleet.create_fleet()
 
     def restart_game(self):
+        self.settings.initialize_dynamic_settings()
         self._reset_level()
         self.ship._center_ship()
         self.game_active = True
@@ -91,6 +94,7 @@ class AlienInvasion:
         self.screen.blit(self.bg, (0,0))
         self.ship.draw()
         self.alien_fleet.draw()
+        #draw HUD
 
         if not self.game_active:
             self.play_button.draw()
