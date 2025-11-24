851a0e37eceffa954cfb3b5e976986b789cf3b76
ChrisTDB <christopher.orta101@gmail.com>
2025-11-24 11:02:28 -0500

    Initial Commit

---
+++ alien_fleet.py
@@ -0,0 +1,107 @@
+import pygame
+from alien import Alien
+from typing import TYPE_CHECKING
+
+if TYPE_CHECKING:
+    from Lab14_corta1 import AlienInvasion
+
+class AlienFleet:
+
+    def __init__(self, game: 'AlienInvasion'):
+        self.game = game
+        self.settings = game.settings
+        self.fleet = pygame.sprite.Group()
+        self.fleet_direction = self.settings.fleet_direction
+        self.fleet_drop_speed = self.settings.fleet_drop_speed
+
+        self.create_fleet()
+
+    def create_fleet(self):
+        alien_h = self.settings.alien_h
+        alien_w = self.settings.alien_w
+        screen_h = self.settings.screen_h
+        screen_w = self.settings.screen_w
+
+        fleet_h, fleet_w = self.calculate_fleet_size(alien_h, screen_h, alien_w, screen_w)
+        x_offset, y_offset = self.calculate_offsets(
+            alien_h, fleet_h, alien_w, fleet_w, screen_h, screen_w
+        )
+        self._create_rectangle_fleet(
+            fleet_w, fleet_h, alien_w, alien_h, x_offset, y_offset
+        )
+
+    def calculate_fleet_size(self, alien_h, screen_h, alien_w, screen_w):
+        fleet_h = (screen_h // alien_h)
+        fleet_w = ((screen_w / 2)// alien_w)
+        if fleet_h % 2 == 0:
+            fleet_h -= 1
+        else:
+            fleet_h -= 2
+
+        if fleet_w % 2 == 0:
+            fleet_w -= 1
+        else:
+            fleet_w -= 2
+
+        return int(fleet_h), int(fleet_w)
+
+    def calculate_offsets(
+        self, alien_h, fleet_h, alien_w, fleet_w, screen_h, screen_w
+    ):
+        half_screen = screen_w // 2
+        fleet_vertical_space = fleet_h * alien_h
+        fleet_horizontal_space = fleet_w * alien_w
+        y_offset = int((screen_h - fleet_vertical_space) // 2)
+        x_offset = half_screen + int((half_screen - fleet_horizontal_space) // 2)
+
+        return x_offset, y_offset
+
+    def _create_rectangle_fleet(
+        self, fleet_w: int, fleet_h: int, alien_w: int, alien_h: int, x_offset: int, y_offset: int
+    ):
+        for col in range(fleet_w):
+            for row in range(fleet_h):
+                current_y = alien_h * row + y_offset
+                current_x = alien_w * col + x_offset
+                if row % 2 == 0 or col % 2 == 0:
+                    continue
+                self._create_alien(current_y, current_x)
+
+    def _create_alien(self, current_x: int, current_y: int):
+        new_alien = Alien(self, current_y, current_x)
+
+        self.fleet.add(new_alien)
+
+    def _check_fleet_edges(self):
+        alien: Alien
+        for alien in self.fleet:
+            if alien.check_edges():
+                self._drop_alien_fleet()
+                self.fleet_direction *= -1
+                break
+
+    def _drop_alien_fleet(self):
+        for alien in self.fleet:
+            alien.x -= self.fleet_drop_speed
+
+    def update_fleet(self):
+        self._check_fleet_edges()
+        self.fleet.update()
+
+    def draw(self):
+        alien: 'Alien'
+        for alien in self.fleet:
+            alien.draw_alien()
+
+    def check_collisions(self, other_group):
+        return pygame.sprite.groupcollide(self.fleet, other_group, True, True)
+
+    def check_fleet_left(self):
+        alien: Alien
+        for alien in self.fleet:
+            if alien.rect.right < 0:
+                return True
+        return False
+
+    def check_destroyed_status(self):
+        return not self.fleet
---
+++ ship.py
@@ -0,0 +1,55 @@
+import pygame
+from typing import TYPE_CHECKING
+
+if TYPE_CHECKING:
+    from Lab14_corta1 import AlienInvasion
+    from arsenal import Arsenal
+
+class Ship:
+    def __init__(self, game: 'AlienInvasion', arsenal: 'Arsenal'):
+        self.game = game
+        self.settings = game.settings
+        self.screen = game.screen
+        self.boundaries = self.screen.get_rect()
+
+        self.image = pygame.image.load(self.settings.ship_file)
+        self.image = pygame.transform.scale(self.image, (self.settings.ship_w, self.settings.ship_h))
+        self.image = pygame.transform.rotate(self.image, -90)
+
+        self.rect = self.image.get_rect()
+        self._center_ship()
+        self.moving_up = False
+        self.moving_down = False
+        self.arsenal = arsenal
+        
+    def _center_ship(self):
+        self.rect.midleft = self.boundaries.midleft
+        self.y = float(self.rect.y)
+
+    def update(self):
+        # updating the position of the ship
+        self._update_ship_movement()
+        self.arsenal.update_arsenal()
+
+    def _update_ship_movement(self):
+        temp_speed = self.settings.ship_speed
+        if self.moving_up and self.rect.top > self.boundaries.top:
+            self.y -= temp_speed
+        if self.moving_down and self.rect.bottom < self.boundaries.bottom:
+            self.y += temp_speed
+        
+        self.rect.y = self.y
+
+    def draw(self):
+        self.arsenal.draw()
+        self.screen.blit(self.image, self.rect)
+
+    def fire(self):
+        return self.arsenal.fire_bullet()
+
+    def check_collisions(self, other_group):
+        if pygame.sprite.spritecollideany(self, other_group):
+            self._center_ship()
+            return True
+        return False
+    
